934d57333c89e54acfcb5dec4fcdb6e9407d3ffa
d.sosinsky <d.sosinsky@duotek.ru>
2022-12-18 15:02:04 +0200

    Divides inputs into separate components, adds forward refs to it

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useContext, useEffect, useReducer, useRef, useState} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
     if (action.type === 'USER_INPUT') {
@@ -35,6 +37,11 @@ const Login = (props) => {
         isValid: undefined
     })
 
+    const authCtx = useContext(AuthContext);
+
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
     // useEffect(() => {
     //     console.log('When mounted')
     //     return () => {
@@ -48,8 +55,7 @@ const Login = (props) => {
     useEffect(() => {
         const identifier = setTimeout(() => {
             console.log('Validity check')
-            setFormIsValid(emailState.isValid && passwordState.isValid
-            )
+            setFormIsValid(emailState.isValid && passwordState.isValid)
         }, 500)
         return () => {
             console.log('CLEANUP');
@@ -91,42 +97,41 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        if (formIsValid) {
+            authCtx.onLogin(emailState.value, passwordState.value);
+        } else if (!emailIsValid) {
+            emailInputRef.current.focus();
+        } else {
+            passwordInputRef.current.focus();
+        }
+
     };
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                <div
-                    className={`${classes.control} ${
-                        emailState.isValid === false ? classes.invalid : ''
-                    }`}
-                >
-                    <label htmlFor="email">E-Mail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={emailState.value}
-                        onChange={emailChangeHandler}
-                        onBlur={validateEmailHandler}
-                    />
-                </div>
-                <div
-                    className={`${classes.control} ${
-                        passwordState.isValid === false ? classes.invalid : ''
-                    }`}
-                >
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={passwordState.value}
-                        onChange={passwordChangeHandler}
-                        onBlur={validatePasswordHandler}
-                    />
-                </div>
+                <Input
+                    id="email"
+                    label="E-Mail"
+                    type="email"
+                    ref={emailInputRef}
+                    value={emailState.value}
+                    isValid={emailIsValid}
+                    onChange={emailChangeHandler}
+                    onBlur={validateEmailHandler}
+                />
+                <Input
+                    id="password"
+                    label="Password"
+                    type="password"
+                    ref={passwordInputRef}
+                    value={passwordState.value}
+                    isValid={passwordIsValid}
+                    onChange={passwordChangeHandler}
+                    onBlur={validatePasswordHandler}
+                />
                 <div className={classes.actions}>
-                    <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+                    <Button type="submit" className={classes.btn}>
                         Login
                     </Button>
                 </div>
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
+    onLogin: (email, password) => {},
+    onLogout: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+        if (storedUserLoggedInInformation) {
+            loginHandler()
+        }
+    }, [])
+
+    const loginHandler = () => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', 'true');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.setItem('isLoggedIn', '');
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+                onLogin: loginHandler,
+                onLogout: logoutHandler,
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>)
+}
 
 export default AuthContext;
